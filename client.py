7a61bacd23d6a474b025418b3cb1fccfeb2b83cc
Rachel Belokopytov <156304605+RachelB9913@users.noreply.github.com>
2024-08-11 16:45:55 +0300

    progress

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ import time
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Server address
-server_address = ('127.0.0.1', 5631)
+server_address = ('127.0.0.1', 5632)
 buffer = b"hello world"
 
 def generate_random_file(filename, size):
@@ -31,22 +31,21 @@ def send_file(filename):
             the_data = f.read(4092)
             if not the_data:
                 client_socket.sendto(the_data, server_address)
-                client_socket.sendto(buffer, server_address)
                 break
             send_time = time.time()  # Record the time before sending each packet
             packet = the_seq_num.to_bytes(4, byteorder='big') + the_data
-            if random.random() > prob:
+            if random.random() > prob:  # defining the probability of the data loss
                 client_socket.sendto(packet, server_address)
                 print('Packet {} sent.'.format(the_seq_num))
+                response, _ = client_socket.recvfrom(4096)  # Receive response from server
+                print(response)
+                recv_time = time.time()  # Record the time after receiving the response
+                rtt = recv_time - send_time  # Calculate the round-trip time
             else:
                 print("packet lost here", str(the_seq_num))
 
-            # response, _ = client_socket.recvfrom(4096)  # Receive response from server
-            # print(response)
-            recv_time = time.time()  # Record the time after receiving the response
-            rtt = recv_time - send_time  # Calculate the round-trip time
             the_seq_num += 1
-        print('File sent successfully.')
+        print('File sent.')
 
 
 # Generate a random file of 10MB
@@ -57,23 +56,28 @@ try:
     send_file(file_name)
     # Receive acknowledgment from server containing list of lost packet sequence numbers
     ack_data, _ = client_socket.recvfrom(4092)
+    print(ack_data)
     if ack_data == b'FIN':
         print("received FIN packet - bye-bye:)")
         client_socket.close()
         exit(0)
 
     lost_packets = [int.from_bytes(ack_data[i:i + 4], byteorder='big') for i in range(0, len(ack_data), 4)]
-    # Resend lost packets
+    # Resend lost packets the same way as above
     print('Resending lost packets:', lost_packets)
     with open(file_name, 'rb') as f:
         for seq_num in lost_packets:
             f.seek(seq_num * 4092)  # Move file pointer to the start of the lost packet
             data = f.read(4092)
-
             resend_packet = seq_num.to_bytes(4, byteorder='big') + data
             client_socket.sendto(resend_packet, server_address)
             print('Resent packet {}.'.format(seq_num))
-
+            response_re, _ = client_socket.recvfrom(4096)  # Receive response from server
+            print(response_re)
+        f.seek(0, 2)
+        the_data = f.read(4092)
+        if not the_data:
+            client_socket.sendto(the_data, server_address)
 
 finally:
     # Close the socket
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the server address
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5631
+UDP_PORT = 5632
 server_address = (UDP_IP, UDP_PORT)
 server_socket.bind(server_address)
 
@@ -30,17 +30,22 @@ def receive_file(filename):
             print(seq_num)
             if seq_num != expected_seq_num:
                 print('Packet loss! Expected number:', expected_seq_num, 'Received number:', seq_num)
-                lost_packets.append(expected_seq_num)  # Store the sequence number of the lost packet
+                # in case of some lost packages in a row add them all to the loss list
+                for i in range(seq_num - expected_seq_num):
+                    lost_packets.append(expected_seq_num + i)  # Store the sequence number of the lost packet
                 losi += 1
                 expected_seq_num += (seq_num - expected_seq_num)
                 # continue  # Skip processing the out-of-order packet
             f.write(data)
-            # print(seq_num)
-            server_socket.sendto(b'ACK', address)  # Send acknowledgment back to client
+            print("here: ", seq_num)
+            message = "ACK " + str(seq_num)
+            ack = message.encode()
+            server_socket.sendto(ack, address)  # Send acknowledgment back to client
             send_time = time.time()  # Record the time after sending the acknowledgment
             rtt = send_time - recv_time  # Calculate the round-trip time
             expected_seq_num += 1
         print(losi)
+        print(len(lost_packets), lost_packets)
         print('File received successfully.')
     if not lost_packets:
         print('No packets are lost.')
@@ -48,9 +53,24 @@ def receive_file(filename):
     else:
         # Convert list of lost packet sequence numbers to bytes
         lost_packets_bytes = b''.join(seq_num.to_bytes(4, byteorder='big') for seq_num in lost_packets)
-
         # Send the list of lost packet sequence numbers back to the client as part of acknowledgment
-        server_socket.sendto(lost_packets_bytes, server_address)
+        server_socket.sendto(lost_packets_bytes, address)
+        # opening the same file again in order to append to it all the lost data
+        with open(filename, 'ab') as f:
+            while True:
+                recv_re_packet, address = server_socket.recvfrom(4096)  # Buffer size is 4096 bytes
+                if not recv_re_packet:
+                    break
+                seq_bytes_re = recv_re_packet[:4]
+                data_re = recv_re_packet[4:]
+                seq_num_re = int.from_bytes(seq_bytes_re, byteorder='big')
+                print("re ", seq_num_re)
+                f.write(data_re)
+                message_re = "ACK"
+                ack_re = message_re.encode()
+                server_socket.sendto(ack_re, address)  # Send acknowledgment back to client
+        print('Lost packets received successfully.')
+
 
 
 # while True:
