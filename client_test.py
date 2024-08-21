94ba67a36608a060cac0b5bf6639093848f833da
NoaFishman <155968680+NoaFishman@users.noreply.github.com>
2024-08-21 11:25:53 +0300

    doko is done
    i did doko to the tests files

--- client_test.py
+++ client_test.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-from client import send_file_time, send_file_packet, generate_random_file  # Ensure that the client module is imported
+from client import send_file_packet, send_file_time, generate_random_file  # Ensure that the client module is imported
 
 
 class Test(unittest.TestCase):
@@ -20,8 +20,8 @@ class Test(unittest.TestCase):
 
     def test_generate_random_file(self):
         # Test file generation
-        generate_random_file(self.file_name, self.file_size)
-        self.assertTrue(os.path.exists(self.file_name))
+        generate_random_file(self.file_name, self.file_size)  # creat random file with our function
+        self.assertTrue(os.path.exists(self.file_name))  # check that the size and the name is like we wanted
         self.assertEqual(os.path.getsize(self.file_name), self.file_size)
         os.remove(self.file_name)  # Clean up
 
@@ -29,10 +29,10 @@ class Test(unittest.TestCase):
         print('test_send_file_packet')
         # Test sending a file with packet loss based on sequence number
         file_name = 'test_file.txt'
-        generate_random_file(file_name, 10 * 1024 * 1024)  # 1MB file
-        send_file_packet(file_name, self.server_address)
+        generate_random_file(file_name, 10 * 1024 * 1024)  # creat 10MB file
+        send_file_packet(file_name, self.server_address)  # call the function and send the server address
 
-        # Check if the file was received correctly
+        # Check if the file was received correctly and the size is at list 10MB
         self.assertTrue(os.path.exists('received_file.txt'))
         self.assertLessEqual(os.path.getsize(file_name), os.path.getsize('received_file.txt'))
 
@@ -41,10 +41,10 @@ class Test(unittest.TestCase):
         print('please be patient it takes some time..')
         # Test sending a file with packet loss based on time
         file_name = 'test_file.txt'
-        generate_random_file(file_name, 10 * 1024 * 1024)  # 1MB file
-        lost_by_time = send_file_time(file_name, self.server_address)
+        generate_random_file(file_name, 10 * 1024 * 1024)  # creat 10MB file
+        lost_by_time = send_file_time(file_name, self.server_address)  # call the function and send the server address
 
-        # Check if the file was received correctly
+        # Check if the file was received correctly and the size is at list 10MB
         self.assertTrue(os.path.exists('received_file.txt'))
         self.assertLessEqual(os.path.getsize(file_name), os.path.getsize('received_file.txt'))
 
--- server_test.py
+++ server_test.py
@@ -15,7 +15,7 @@ class TestUDPServer(unittest.TestCase):
             os.remove('test_with_loss.txt')
 
     @patch('socket.socket')
-    def test_receive_file_no_loss(self, mock_socket):
+    def test_receive1(self, mock_socket):
         # Simulate receiving packets in sequence without loss
         mock_socket_instance = mock_socket.return_value
         mock_socket_instance.recvfrom.side_effect = [
@@ -24,29 +24,34 @@ class TestUDPServer(unittest.TestCase):
             (b'\x00\x00\x00\x02' + b'data3', ('127.0.0.1', 5632)),
             (b'', ('127.0.0.1', 5632)),  # End of file
         ]
+        # call the receive1 function that check by seq num that all the packets arrived, return the lost packets seq num
         lost_packets = receive1('test_no_loss.txt', mock_socket_instance)
         with open('test_no_loss.txt', 'rb') as f:
             content = f.read()
+            # check that the data we got contain the data we send
             self.assertEqual(content, b'data1data2data3')
 
     @patch('socket.socket')
-    def test_receive_file_with_loss(self, mock_socket):
+    def test_receive1_loss(self, mock_socket):
         # Simulate receiving packets with loss
         mock_socket_instance = mock_socket.return_value
+        # sending 2 packets that their seq num are non-consecutive
         mock_socket_instance.recvfrom.side_effect = [
             (b'\x00\x00\x00\x00' + b'data1', ('127.0.0.1', 5632)),
             (b'\x00\x00\x00\x02' + b'data3', ('127.0.0.1', 5632)),
             (b'', ('127.0.0.1', 5632)),  # End of file
         ]
+        # calling the receive1 function and want to get a list that say that packet with seq num 1 is lost
         lost_packets = receive1('test_with_loss.txt', mock_socket_instance)
         self.assertEqual(lost_packets, [1])
 
         with open('test_with_loss.txt', 'rb') as f:
             content = f.read()
+            # check that the data we got contain the data we send
             self.assertEqual(content, b'data1data3')
 
     @patch('socket.socket')
-    def test_receive_file_acknowledgment(self, mock_socket):
+    def test_receive2(self, mock_socket):
         # Simulate acknowledgment handling
         mock_socket_instance = mock_socket.return_value
         mock_socket_instance.recvfrom.side_effect = [
@@ -54,31 +59,34 @@ class TestUDPServer(unittest.TestCase):
             (b'\x00\x00\x00\x01' + b'data2', ('127.0.0.1', 5632)),
             (b'', ('127.0.0.1', 5632)),  # End of file
         ]
+        # sent 2 packets and want to check that the data we sent is now in the new file
         with patch('builtins.open', unittest.mock.mock_open()) as mock_file:
             receive2('test_ack.txt', mock_socket_instance)
             mock_file().write.assert_any_call(b'data1')
             mock_file().write.assert_any_call(b'data2')
+        # check that we got ack for each packet we sent
         mock_socket_instance.sendto.assert_any_call(b'ACK ', ('127.0.0.1', 5632))
 
     @patch('socket.socket')
-    def test_receive_file_acknowledgment_loss(self, mock_socket):
-        # Simulate acknowledgment handling !!!!!!!!!!!!!!!!
+    def test_receive3(self, mock_socket):
+        # Simulate acknowledgment handling and packet lost
         mock_socket_instance = mock_socket.return_value
+        # sending 2 packets that their seq num are non-consecutive
         mock_socket_instance.recvfrom.side_effect = [
             (b'\x00\x00\x00\x00' + b'data1', ('127.0.0.1', 5632)),
             (b'\x00\x00\x00\x02' + b'data3', ('127.0.0.1', 5632)),
             (b'', ('127.0.0.1', 5632)),  # End of file
         ]
+        # in receive3 we look for loss packets by time and seq num so we combin the last 2 tests
         with patch('builtins.open', unittest.mock.mock_open()) as mock_file:
             lost_list = receive3('test_ack_loss.txt', mock_socket_instance)
+            # check that the data i in the new file
             mock_file().write.assert_any_call(b'data1')
             mock_file().write.assert_any_call(b'data3')
-
+        # check that we sent ack for each packet
         mock_socket_instance.sendto.assert_any_call(b'ACK ', ('127.0.0.1', 5632))
-
+        # check that the function realize that we "lost" packet with seq num 1
         self.assertEqual(lost_list, [1])
-
-
 
 
 if __name__ == '__main__':
